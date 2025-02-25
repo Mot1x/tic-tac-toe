195bf40bb2e93d88c2391b116f92c346d86d99f3
Mot1x <motixflawless@gmail.com>
2025-02-25 18:41:26 +0500

    бу

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
 
+const winLines = [
+    (0, 1, 2), (3, 4, 5), (6, 7, 8), 
+    (0, 3, 6), (1, 4, 7), (2, 5, 8), 
+    (0, 4, 8), (2, 4, 6)
+]
+var field = [
+    [' ', ' ', ' '], 
+    [' ', ' ', ' '], 
+    [' ', ' ', ' ']
+]
+
+var stepCount = 0;
+var isGameEnded = false;
 const container = document.getElementById('fieldWrapper');
 
 startGame();
@@ -27,13 +40,17 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    if (field[row][col] !== EMPTY)
+        return;
+
+    field[row][col] = stepCount % 2 === 0 ? CROSS : ZERO;
+    stepCount++;
     console.log(`Clicked on cell: ${row}, ${col}`);
+    renderSymbolInCell(field[row][col], row, col);
 
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (stepCount === 9)
+        alert('Победила дружба');
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -41,6 +58,18 @@ function renderSymbolInCell (symbol, row, col, color = '#333') {
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
+}
+
+function paintWinCells (){
+    for (const line of winLines){
+        
+        if (field[Math.floor(line[0] / 3)][line[0] % 3] != EMPTY &&
+            field[Math.floor(line[0] / 3)][line[0] % 3] === field[Math.floor(line[1] / 3)][line[1] % 3] === field[Math.floor(line[2] / 3)][line[2] % 3]){
+            findCell(Math.floor(line[0] / 3), line[0] % 3).style.color = 'red';
+            findCell(Math.floor(line[1] / 3), line[1] % 3).style.color = 'red';
+            findCell(Math.floor(line[2] / 3), line[2] % 3).style.color = 'red';
+        }
+    }
 }
 
 function findCell (row, col) {
